2cd201ffa0df14d8b9bbb9bcf1de04b68b4c7cf9
Robbe Buyle <robbebuyle@Robbes-MacBook-Pro.local>
2019-11-12 22:09:07 +0100

    init first Flutter test

---
+++ tests/basics/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() {
+  // print('Hello');
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text('Hey!'),
+    ),
+    body: Text('This is some text. Welcome to my app!'),
+  )));
+}
